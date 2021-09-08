9a835807710a89a287239fb180b2079b12c627fd
Ihor_Kozar <ihor_kozar@epam.com>
2021-09-08 10:30:31 +0300

    remove CoroutineCallAdapterFactory() and LocalBroadcastManager restoring order in the OverviewFragment.kt

--- app/src/main/java/com/example/weather/network/ApiService.kt
+++ app/src/main/java/com/example/weather/network/ApiService.kt
@@ -1,13 +1,12 @@
 package com.example.weather.network
 
-import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
     @GET("data/2.5/weather")
-    fun getPostResponseAsync(
+    suspend fun getPostResponseAsync(
         @Query("q") cityName: String,
         @Query("appid") apiKey: String
-    ): Deferred<NetworkPostResponse>
+    ): NetworkPostResponse
 }
